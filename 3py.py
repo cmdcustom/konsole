d2d7b65be8fb3b73a471a79aca60f85a708ed929
cmdcustom <76268123+cmdcustom@users.noreply.github.com>
2021-03-18 19:49:55 -0600

    Created by bot

---
+++ 3py.py
@@ -0,0 +1,11 @@
+import requests
+
+print('\n')
+
+while 1:
+    cmd = input('>> ')
+    if cmd != '/e/':
+        r = requests.get(f'https://shle.cmdcustom.repl.co?lang=python3&cmd={cmd}')
+        print(r.content.decode())
+    else:
+        exit()
