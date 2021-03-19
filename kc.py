f2acba0bc99803c9d601908257f95265fdcde386
cmdcustom <76268123+cmdcustom@users.noreply.github.com>
2021-03-18 19:49:58 -0600

    Created by bot

---
+++ kc.py
@@ -0,0 +1,14 @@
+import sys
+import requests
+
+if sys.argv[2] == 'csharp':
+    print(requests.get(f'https://konsolecompile.cmdcustom.repl.co?ext=cs&data={open(sys.argv[1]).read()}').content.decode())
+if sys.argv[2] == 'python':
+    print(requests.get(f'https://konsolecompile.cmdcustom.repl.co?ext=py&data={open(sys.argv[1]).read()}').content.decode())
+if sys.argv[2] == 'python2':
+    print(requests.get(f'https://konsolecompile.cmdcustom.repl.co?ext=py2&data={open(sys.argv[1]).read()}').content.decode())
+if sys.argv[2] == 'bash':
+    print(requests.get(f'https://konsolecompile.cmdcustom.repl.co?ext=sh&data={open(sys.argv[1]).read()}').content.decode())
+if sys.argv[2] == 'nodejs':
+    print(requests.get(f'https://konsolecompile.cmdcustom.repl.co?ext=js&data={open(sys.argv[1]).read()}').content.decode())
+    
